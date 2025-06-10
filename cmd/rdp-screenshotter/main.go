f161a38d8d54b80da236b004d081162dd37991e0
Pepijn van der Stap <pepijn@korper.nl>
2025-06-10 08:21:31 +0200

    typos

--- cmd/rdp-screenshotter/main.go
+++ cmd/rdp-screenshotter/main.go
@@ -1,5 +1,5 @@
-// RDP Screenshotter - Capture screenshots from RDP servers
-// Copyright (C) 2024 RDP Screenshotter Contributors
+// RDP Screenshotter Go - Capture screenshots from RDP servers
+// Copyright (C) 2025 - Pepijn van der Stap, pepijn@neosecurity.nl
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
